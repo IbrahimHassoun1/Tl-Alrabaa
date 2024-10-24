79a46d29c351259ee1ec0d4a49c300f1ccf1b66f
Tl-Alrabaa <ihassoun73@gmail.com>
2024-10-24 09:18:06 -0700

    footer fixed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 React
 const Footer = () => {
+  const [dropDownLocation,setDropDownLocation]=useState(false)
   return (
     <div className='footer mt-3 bg-primary min-h-52'>
         <div className='containers pt-5 m-auto w-full flex flex-wrap justify-between text-start text-tertiary'>
@@ -13,7 +14,20 @@ const Footer = () => {
                 <ul className='pt-5 text-center font-extralight'>
                   <li className='cursor-pointer'>About Us</li>
                   <li className='cursor-pointer'>Terms&Conditions</li>
-                  <li className='cursor-pointer'>Locations</li>
+                  <li className='cursor-pointer'>
+                    
+                  <span className='cursor-pointer' onClick={() => setDropDownLocation(!dropDownLocation)}>
+                Locations 
+                <span className={`font-thin text-xs ml-1 transition-transform duration-300 ${dropDownLocation ? 'rotate-180' : ''}`}>&#9660;</span>
+              </span> <br />
+              <ul
+                className={`ml-auto mr-8 w-2/5  border-l border-l-gray-100 transition-max-height duration-300 ease-in-out overflow-hidden ${dropDownLocation ? 'max-h-40' : 'max-h-0'}`}
+              >
+               
+                <li className={`pl-2 cursor-pointer `} ><a href="https://maps.app.goo.gl/VCXNQvV7oeZeJc1p6">Location 1</a></li>
+                <li className={`pl-2 cursor-pointer`} ><a href="">Location 2</a></li>
+              </ul>
+                  </li>
                 </ul>
             </div>
             <div className="right w-full md:w-1/5 text-center">
