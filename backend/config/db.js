b49db9fcca666cf8ae1d97c0ee8df5d106c5e997
Tl-Alrabaa <ihassoun73@gmail.com>
2024-10-24 05:05:06 -0700

    added mongoURI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://Tl-Alrabaa:Tl-Alrabaa%401962@tl-alrabaa.ebxny.mongodb.net/?retryWrites=true&w=majority&appName=Tl-Alrabaa");
+        await mongoose.connect(process.env.MONGO_URI||"mongodb+srv://Tl-Alrabaa:Tl-Alrabaa%401962@tl-alrabaa.ebxny.mongodb.net/?retryWrites=true&w=majority&appName=Tl-Alrabaa");
         console.log("DB connected successfully");
     } catch (error) {
         console.error("Connection error:", error);
