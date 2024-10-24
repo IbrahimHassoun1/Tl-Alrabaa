da352abebc765ffde2bb2eedfc50348c54be18d1
Tl-Alrabaa <ihassoun73@gmail.com>
2024-10-24 03:14:38 -0700

    main Cors added

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app=express()
+app.use(cors({
+    origin:"",
+    methods:["POST","GET","DELETE","PATCH"],
+    credentials:true
+}))
+
+
 const PORT = process.env.PORT || 5000;
 
 //middlewares
