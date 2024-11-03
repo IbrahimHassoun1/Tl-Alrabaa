0d7edb269a7ab8490e61994babea2b445e4fa13f
Tl-Alrabaa <ihassoun73@gmail.com>
2024-11-03 03:32:40 -0800

    fixed footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
 React
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'; // Import the Facebook icon
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'; // Import the Facebook icon
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'; 
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'; 
 const Footer = () => {
   const [dropDownLocation,setDropDownLocation]=useState(false)
+  
   return (
     <div className='footer mt-3 bg-primary min-h-52'>
         <div className='containers pt-5 m-auto w-full flex flex-wrap justify-between text-start text-tertiary'>
             <div className="left w-full text-center md:w-3/5 md:text-start">
                 <h1 className='font-extrabold text-4xl text-secondary'>Tl-Alrabaa</h1>
-                <p className='pt-5 font-extralight '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi perspiciatis modi deserunt repellat! Beatae nihil minima modi vero rerum tenetur iusto, iure iste quod quam cupiditate, laboriosam facilis cum dolore!</p>
+                <p className='pt-5 font-extralight '>Tl-Alrabaa, based in Erbil, Iraq, is a vibrant tobacco company that marries tradition with innovation to craft exceptional tobacco products. Renowned for its rich flavors and meticulous craftsmanship, Tl-Alrabaa appeals to shisha enthusiasts and tobacco lovers alike, offering a unique cultural experience in every puff.
+
+
+</p>
             </div>
             <div className="center w-full md:w-1/5 ">
                 <h1 className='font-extrabold text-3xl text-center'>Company</h1>
@@ -35,6 +44,13 @@ const Footer = () => {
                 <ul className='pt-5 font-extralight'>
                   <li className='cursor-pointer'><a href="mailto:Tlalrabaa2021@gmail.com">Email</a></li>
                   <li className='cursor-pointer'><a href="tel:+964-0750-546-2539">Phone Number</a></li>
+                  <li className='flex containers justify-around text-2xl w-full'>
+                    <a href='https://www.facebook.com/p/Tl-Alrabaa-100077383218803/' target='_blank'><FontAwesomeIcon icon={faFacebook} /></a>
+                    <a target='_blank' href='https://www.instagram.com/tl_alrabaa/p/C9DFfvCulij/'><FontAwesomeIcon icon={faInstagram} /></a>
+                    <a target='_blank' href="tel:+964-0750-546-2539"><FontAwesomeIcon icon={faWhatsapp} /></a>
+                    <a target='_blank' ><FontAwesomeIcon icon={faLinkedin} /></a>
+                  </li>
+
                 </ul>
             </div>
         </div>
