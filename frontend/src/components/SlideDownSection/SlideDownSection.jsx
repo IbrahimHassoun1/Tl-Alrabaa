5c6e9b1648011b0d3220a8065dba179a9489b71c
Tl-Alrabaa <ihassoun73@gmail.com>
2024-11-03 03:01:05 -0800

    removed slide css

--- frontend/src/components/SlideDownSection/SlideDownSection.jsx
+++ frontend/src/components/SlideDownSection/SlideDownSection.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 React
 import { useInView } from 'react-intersection-observer';
-import './SlideDownSection.css'; // Custom CSS for slide-down effect
 import PropTypes from 'prop-types';
 
 const SlideDownSection = ({ children, className }) => {
   const { ref, inView } = useInView({ threshold: 0.1, triggerOnce: true });
 
+  const styles = {
+    opacity: inView ? 1 : 0,
+    transform: inView ? 'translateY(0)' : 'translateY(-20px)', // Slide effect
+    transition: 'opacity 1s ease, transform 1s ease',
+  };
+
   return (
     <div 
       ref={ref} 
-      className={`${className} slide-down-section ${inView ? 'visible' : ''}`}
+      className={`${className} slide-down-section`} 
+      style={styles}
     >
       {children}
     </div>
